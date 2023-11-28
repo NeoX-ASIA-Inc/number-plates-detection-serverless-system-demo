fe83a7680ef5389bf25ffe26efaa887a772f0933
Hoang Duc Quan <rocker.gryphon@gmail.com>
2023-11-29 00:12:37 +0900

    Add S3 resource (#1)
    * Add S3 resource
    
    * add file upload
    
    * complete code
    
    ---------
    
    Co-authored-by: Hoang Duc Quan <quanhd@neox-asia.co.jp>

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-from flask import Flask, jsonify, make_response
+import boto3
+import uuid
+import os
+
+from flask import Flask, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+ALLOWED_EXTENSIONS = {'jpg', 'png'}
 
-@app.route("/")
-def hello_from_root():
-    return jsonify(message='Hello from root!')
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET', 'POST'])
+def root():
+    if request.method == 'POST':
+        uploaded_file = request.files['file-to-upload']
+        if not allowed_file(uploaded_file.filename):
+            return 'FILE NOT ALLOWED!'
 
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
+        new_filename = uuid.uuid4().hex + '.' + uploaded_file.filename.rsplit('.', 1)[1].lower()
 
+        bucket_name = os.environ['BUCKET']
+        s3 = boto3.resource('s3')
+        s3.Bucket(bucket_name).upload_fileobj(uploaded_file, new_filename)
 
-@app.errorhandler(404)
-def resource_not_found(e):
-    return make_response(jsonify(error='Not found!'), 404)
+        return render_template('index.html', file=new_filename)
+    return render_template('index.html', file='')
